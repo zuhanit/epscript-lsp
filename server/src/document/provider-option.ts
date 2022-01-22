350b37152a4802edc1bf0b16cd00491c641039e5
zuhanit <36349353+zuhanit@users.noreply.github.com>
2022-01-22 16:34:37 +0900

    Add ProviderOption interface for convenience

---
+++ server/src/document/provider-option.ts
@@ -0,0 +1,7 @@
+import { TextDocumentPositionParams } from 'vscode-languageserver';
+import { ContextPackage } from '../context/IContextPackage';
+
+export interface ProviderOption<T extends TextDocumentPositionParams> {
+	params: T;
+	contextPackage: ContextPackage;
+}
