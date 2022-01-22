cf2974b49d9abe36cab328ff4e2349bd80240342
zuhanit <36349353+zuhanit@users.noreply.github.com>
2022-01-22 16:36:12 +0900

    Implement search methods

--- server/src/analyzer.ts
+++ server/src/analyzer.ts
@@ -1,5 +1,10 @@
+import { ParserRuleContext } from 'antlr4ts';
+import { ParseTree } from 'antlr4ts/tree/ParseTree';
+import { TerminalNode } from 'antlr4ts/tree/TerminalNode';
 import { Connection, InitializeParams, URI, WorkspaceFolder } from 'vscode-languageserver';
-import { IContextPackage } from './context/IContextPackage';
+import { Position, TextDocument } from 'vscode-languageserver-textdocument';
+import { ContextPackage } from './context/IContextPackage';
+import { Parser } from './parser';
 
 /**
  * 문서 정리 프로그램.
@@ -8,7 +13,12 @@ export class Analyzer {
 	/**
 	 * 클라이언트에 열려있는 문서 패키지.
 	 */
-	documents = new Map<URI, IContextPackage>();
+	public documents = new Map<URI, ContextPackage>();
+	public parser: Parser;
+
+	constructor(parser: Parser) {
+		this.parser = parser;
+	}
 
 	/**
 	 * 분석기 초기화 및 호출.
@@ -19,23 +29,84 @@ export class Analyzer {
 		(
 			connection: Connection,
 			params: InitializeParams,
+			parser: Parser,
 		): Promise<Analyzer>
 	{
+		// params.workspaceFolders
 		console.log('Analyzer Initialized');
-		const analyzer = new Analyzer();
+		const analyzer = new Analyzer(parser);
 		return analyzer;
 	}
 
 	public async analyze
 		(
 			uri: URI,
-			document: string
-		): Promise<IContextPackage>
+			document: TextDocument,
+		): Promise<ContextPackage>
 	{
-		const contextPackage: IContextPackage = {
+		const contextPackage: ContextPackage = {
 			document: document,
+			parsePackage: this.parser.parse(document),
 		};
 		this.documents.set(uri, contextPackage);
 		return contextPackage;
+	}
+
+	public getNodeAtPosition
+		(
+			uri: URI,
+			position: Position,
+		)
+	{
+		const ast = this.documents.get(uri)?.parsePackage.ast;
+		if (ast === undefined) return null;
+		console.log(ast);
+		return this.parseTreeFromPosition(ast, position.character, position.line + 1);
+	}
+
+	public parseTreeFromPosition = (root: ParseTree, column: number, row: number): ParseTree | undefined => {
+		// Does the root node actually contain the position? If not we don't need to look further.
+		if (root instanceof TerminalNode) {
+			const terminal = (root);
+			const token = terminal.symbol;
+			if (token.line !== row) { return undefined; }
+	
+			const tokenStop = token.charPositionInLine + (token.stopIndex - token.startIndex + 1);
+			if (token.charPositionInLine <= column && tokenStop >= column) {
+				return terminal;
+			}
+	
+			return undefined;
+		} else {
+			const context = (root as ParserRuleContext);
+			if (!context.start || !context.stop) { // Invalid tree?
+				return undefined;
+			}
+	
+			if (context.start.line > row || (context.start.line === row && column < context.start.charPositionInLine)) {
+				return undefined;
+			}
+	
+			const tokenStop = context.stop.charPositionInLine + (context.stop.stopIndex - context.stop.startIndex + 1);
+			if (context.stop.line < row || (context.stop.line === row && tokenStop < column)) {
+				return undefined;
+			}
+	
+			if (context.children) {
+				for (const child of context.children) {
+					const result = this.parseTreeFromPosition(child, column, row);
+					if (result) {
+						return result;
+					}
+				}
+			}
+	
+			return context;
+	
+		}
+	};
+
+	public getWordAtPosition() {
+		//
 	}
 }
--- server/src/context/symbolTable/SymbolTable.ts
+++ server/src/context/symbolTable/SymbolTable.ts
@@ -1,10 +1,14 @@
+import { Position } from 'vscode-languageserver';
+import { zeroRange } from '../../util/range';
+import { BaseScope } from './BaseScope';
+import { BaseSymbol } from './BaseSymbol';
 import { GlobalScope } from './GlobalScope';
 import { ISymbol } from './ISymbol';
 import { PredefinedScope } from './PredefinedScope';
 
 export class SymbolTable {
-	predefinedScope: PredefinedScope = new PredefinedScope();
-	globalScope: GlobalScope = new GlobalScope(this.predefinedScope);
+	predefinedScope: PredefinedScope = new PredefinedScope(zeroRange);
+	globalScope: GlobalScope = new GlobalScope(zeroRange, this.predefinedScope);
 
 	insertPredefinedSymbol(symbol: ISymbol) {
 		this.predefinedScope.insert(symbol);
@@ -12,5 +16,16 @@ export class SymbolTable {
 
 	insertGlobalSymbol(symbol: ISymbol) {
 		this.globalScope.insert(symbol);
+	}
+
+	/**
+	 * 편의 목적 함수들.
+	 */
+	getSymbolByName(name: string): BaseSymbol | undefined {
+		console.log(this.predefinedScope);
+		let result = this.globalScope.getAllSymbols().find(symbol => symbol.name === name) as BaseSymbol;
+		// PredfinedSymbol인지 확인.
+		if (result === undefined) result = this.predefinedScope.getAllSymbols().find(symbol => symbol.name === name) as BaseSymbol;
+		return result;
 	}
 }
