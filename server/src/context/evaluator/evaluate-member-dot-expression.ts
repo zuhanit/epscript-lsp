6cba5d4c9bf4cf3b7704b2ca3ea713ce5c2c0fe4
zuhanit <36349353+zuhanit@users.noreply.github.com>
2022-01-22 16:28:56 +0900

    Add type checking of VariableSymbol

--- server/src/context/evaluator/evaluate-member-dot-expression.ts
+++ server/src/context/evaluator/evaluate-member-dot-expression.ts
@@ -1,12 +1,14 @@
 import { type } from 'os';
 import { MemberDotExpressionContext } from '../../grammar/epScriptParser';
 import { BaseScope } from '../symbolTable/BaseScope';
+import { MemberSymbol } from '../symbolTable/MemberSymbol';
+import { VariableSymbol } from '../symbolTable/VariableSymbol';
 import { evaluateNode } from './evaluator';
 import { EvaluatorOption } from './evaluator-options';
 
 export function evaluateMemberDotExpression({node, ...rest}: EvaluatorOption<MemberDotExpressionContext>) {
-	const expressionResult = evaluateNode({node: node.singleExpression(), ...rest});
-	
+	let expressionResult = evaluateNode({node: node.singleExpression(), ...rest});
+	if (expressionResult instanceof VariableSymbol || expressionResult instanceof MemberSymbol) expressionResult = expressionResult.value;
 	const match = expressionResult instanceof BaseScope
 		? expressionResult.getSymbolByName(node.identifier().text)
 		: undefined;
