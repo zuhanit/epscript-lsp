8ebcf20b846eaaf612d668d0aaf69f7848517792
zuhanit <36349353+zuhanit@users.noreply.github.com>
2022-02-10 15:56:19 +0900

    Seperate range util

---
+++ server/src/util/range.ts
@@ -0,0 +1,33 @@
+import { ParserRuleContext } from 'antlr4ts';
+import { Range } from 'vscode-languageserver';
+
+export const zeroRange: Range = {
+	start: {
+		line: 0,
+		character: 0,
+	},
+	end: {
+		line: 0,
+		character: 0
+	}
+};
+
+/**
+ * 컨텍스트로부터 `Range` 얻어오기.
+ * 
+ * @param ctx 파서 컨텍스트
+ * @returns Range
+ */
+export function getRangeByContext(ctx: ParserRuleContext): Range {
+	const result: Range = {
+		start: {
+			line: ctx.start.line-1,
+			character: ctx.start.charPositionInLine,
+		},
+		end: {
+			line: ctx.stop!.line-1,
+			character: ctx.stop!.charPositionInLine + ctx.stop!.stopIndex - ctx.stop!.startIndex,
+		}
+	};
+	return result;
+}
