67e6b979a15c0f569e2617b8f8ba4fc02b4d1f38
zuhanit <36349353+zuhanit@users.noreply.github.com>
2022-02-10 16:02:15 +0900

    Using analyzer for unit test

---
+++ server/src/test/tester.test.ts
@@ -0,0 +1,16 @@
+import { Analyzer } from '../analyzer';
+import { Parser } from '../parser';
+import { FIXTURES_DOCUMENT, FIXTURES_URI } from './fixtures';
+
+const parser = Parser.initialize();
+const analyzer = new Analyzer(parser);
+
+describe('Class Test', () => {
+	// const result = analyzer.analyze(FIXTURES_URI.CLASS, FIXTURES_DOCUMENT.CLASS);
+	// console.log(result);
+});
+
+describe('Module Test', () => {
+	// const result = analyzer.analyze(FIXTURES_URI.MODULE, FIXTURES_DOCUMENT.MODULE);
+	// console.log(result);
+});
