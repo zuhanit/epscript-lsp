90f07133cce9770f57ac3d5b52a393e30e2cde5c
zuhanit <36349353+zuhanit@users.noreply.github.com>
2022-02-11 02:13:22 +0900

    Match ANTLR line with VSCode line

--- server/src/diagnostic/DiagnosticManager.ts
+++ server/src/diagnostic/DiagnosticManager.ts
@@ -10,15 +10,15 @@ export function pushDiagnostic(message: string, diagnostic: Diagnostic[], t: Par
 		range: {
 			start: {
 				character: t.start.charPositionInLine,
-				line: t.start.line,
+				line: t.start.line - 1,
 			},
 			end: {
 				character: t.stop && t.stop.text
 					? t.stop.charPositionInLine + t.stop.text?.length
 					: t.start.charPositionInLine,
 				line: t.stop
-					? t.stop.line
-					: t.start.line
+					? t.stop.line - 1
+					: t.start.line - 1
 			}
 		}
 	});
