2c8dd10bb73f2044b6e84cfbe6d0bb11e3777f32
zuhanit <36349353+zuhanit@users.noreply.github.com>
2022-02-10 15:57:29 +0900

    Implement fake evaluate

--- server/src/context/evaluator/evaluate-binary-expression.ts
+++ server/src/context/evaluator/evaluate-binary-expression.ts
@@ -1,7 +1,32 @@
-import { BinaryExpressionContext } from '../../grammar/epScriptParser';
+import { BinaryExpressionContext } from '../../grammar/src/grammar/lib/epScriptParser';
+import { epScriptParser } from '../../grammar/src/grammar/lib/epScriptParser';
+import { ParsePackage } from '../../parser';
+import { BaseScope } from '../symbolTable/BaseScope';
+import { BaseSymbol } from '../symbolTable/BaseSymbol';
+import { SymbolTable } from '../symbolTable/SymbolTable';
+import { VariableSymbol } from '../symbolTable/VariableSymbol';
 import { evaluateNode } from './evaluator';
 import { EvaluatorOption } from './evaluator-options';
+import { Literal } from './literal';
 
 export function evaluateBinaryExpression({node, ...rest}: EvaluatorOption<BinaryExpressionContext>) {
-	return evaluateNode({node: node, ...rest});
+	const left = evaluateNode({node: node.singleExpression(0), ...rest});
+	const right = evaluateNode({node: node.singleExpression(1), ...rest});
+
+	switch (node.binaryOperator().start.type) {
+		case epScriptParser.Assign:
+			if (left) {
+				setValue(left, right);
+				return right;
+			}
+			break;
+		default:
+			return left;
+	}
+
+	return undefined;
+}
+
+function setValue(symbol: Literal, value: Literal): void {
+	if (symbol instanceof VariableSymbol) symbol.value = value;
 }
