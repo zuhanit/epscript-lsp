a17880d1359198b3dcddd61b3457d9605b32f121
zuhanit <36349353+zuhanit@users.noreply.github.com>
2022-02-10 16:08:19 +0900

    Refactored features to using SymbolInfo

--- server/src/document/hover.ts
+++ server/src/document/hover.ts
@@ -1,24 +1,16 @@
 import { Hover, HoverParams, MarkupContent } from 'vscode-languageserver';
-import { BaseScope } from '../context/symbolTable/BaseScope';
+import { getSymbolInfo } from '../context/facade';
 import { BaseSymbol } from '../context/symbolTable/BaseSymbol';
-import { ClassSymbol } from '../context/symbolTable/ClassSymbol';
-import { FunctionSymbol } from '../context/symbolTable/FunctionSymbol';
-import { ISymbol } from '../context/symbolTable/ISymbol';
-import { MemberSymbol } from '../context/symbolTable/MemberSymbol';
-import { MethodSymbol } from '../context/symbolTable/MethodSymbol';
-import { ParameterSymbol } from '../context/symbolTable/ParameterSymbol';
-import { VariableSymbol } from '../context/symbolTable/VariableSymbol';
-import { literalToType } from '../util/literalUtils';
 import { ProviderOption } from './provider-option';
 
-export function provideHoverItem({params, contextPackage}: ProviderOption<HoverParams>, name: string): Hover | undefined {
+
+export function provideHoverItem({params, contextPackage, name}: ProviderOption<HoverParams>): Hover | undefined {
 	const symbolTable = contextPackage.parsePackage.symbolTable;
 	const symbol = symbolTable.getSymbolByName(name);
 	let content: MarkupContent | undefined;
 
+	console.log(symbol);
 	if (symbol) content = getDocumentationForSymbol(symbol);
-
-	console.log(params, symbol, content);
 
 	if (content) {
 		return {
@@ -37,26 +29,9 @@ export function provideHoverItem({params, contextPackage}: ProviderOption<HoverP
  */
 function getDocumentationForSymbol(symbol: BaseSymbol): MarkupContent {
 	const value: string[] = ['```epscript'];
-	if (symbol instanceof VariableSymbol) {
-		if (symbol instanceof MemberSymbol) {
-			value.push(`(property) ${symbol.scope.name}.${symbol.name}: ${literalToType(symbol.value)}`);
-		} else {
-			value.push(`${symbol.modifier} ${symbol.name}: ${symbol.value ? literalToType(symbol.value) : 'undefined'}`);
-		}
-	}
-
-	if (symbol instanceof FunctionSymbol) {
-		const args = symbol.getSymbols().filter<ParameterSymbol>(isParameterSymbol).map(x => `${x.name}: ${literalToType(x.type)}`);
-		if (symbol instanceof MethodSymbol) {
-			value.push(`(method) ${symbol.name}(${args.join(', ')}): ${literalToType(symbol.retType)}`);
-		} else {
-			value.push(`function ${symbol.name}(${args.join(', ')}): ${literalToType(symbol.retType)}`);
-		}
-	}
-
-	if (symbol instanceof ClassSymbol) {
-		value.push(`class ${symbol.name}`);
-	}
+	
+	const info = getSymbolInfo(symbol);
+	value.push(info.detail);
 
 	value.push('```');
 
@@ -64,8 +39,4 @@ function getDocumentationForSymbol(symbol: BaseSymbol): MarkupContent {
 		kind: 'markdown',
 		value: value.join('\n')
 	};
-}
-
-function isParameterSymbol(symbol: ISymbol): symbol is ParameterSymbol {
-	return symbol instanceof ParameterSymbol;
 }
