4d436a18f6a8328c8ea38e6783c9a310427465d6
zuhanit <36349353+zuhanit@users.noreply.github.com>
2022-02-10 16:13:40 +0900

    Add parsePackage and workspaceFolder

--- server/src/context/IContextPackage.ts
+++ server/src/context/IContextPackage.ts
@@ -1,3 +1,9 @@
-export interface IContextPackage {
-	document: string;
+import { TextDocument } from 'vscode-languageserver-textdocument';
+import { ParsePackage } from '../parser';
+import { SymbolTable } from './symbolTable/SymbolTable';
+
+export interface ContextPackage {
+	document: TextDocument;
+	parsePackage: ParsePackage;
+	workspaceFolder: string,
 }
