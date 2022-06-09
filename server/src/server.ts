ec67cbf74ced289b6e19fbc01ae5cffb3f1cbbe9
zuhanit <36349353+zuhanit@users.noreply.github.com>
2022-06-10 02:31:39 +0900

    feat: Add process handlers

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,33 +1,51 @@
 import {
-	createConnection,
-	Connection,
-	InitializeParams,
-	InitializeResult,
-	ProposedFeatures,
-	CancellationToken,
-	CompletionItem,
-	CompletionParams,
-	TextDocumentSyncKind,
-} from 'vscode-languageserver/node';
+  createConnection,
+  Connection,
+  InitializeParams,
+  InitializeResult,
+  ProposedFeatures,
+  CancellationToken,
+  CompletionItem,
+  CompletionParams,
+  TextDocumentSyncKind,
+} from "vscode-languageserver/node";
 
-import { EPSServer } from './epsServer';
+import { EPSServer } from "./epsServer";
 
 // eslint-disable-next-line prefer-const
 let connection: Connection = createConnection(ProposedFeatures.all);
 
-// 초기화시 일어날 핸들러 설정.
-connection.onInitialize(async (params: InitializeParams): Promise<InitializeResult> => {
-	connection.console.log('epScript Server initializing...');
-	const startTime = Date.now();
-	
-	const server = await EPSServer.initialize(connection, params);
-	server.register(connection);
+process.exit = ((code?: number): void => {
+  void connection.sendNotification("Server exitted, code", code);
+}) as any;
 
-	console.log(`Server registered, passed time: ${(Date.now() - startTime) / 1000} seconds`);
-
-	return {
-		capabilities: server.capabilities()
-	};
+process.on("uncaughtException", (error: any) => {
+  console.error("Uncaught exception received.");
+  if (error) {
+    console.log(error);
+  }
 });
+
+// 초기화시 일어날 핸들러 설정.
+connection.onInitialize(
+  async (params: InitializeParams): Promise<InitializeResult> => {
+    connection.console.log("epScript Server initializing...");
+    const startTime = Date.now();
+
+    const server = await EPSServer.initialize(connection, params);
+    server.register(connection);
+
+    console.log(
+      `Server registered, passed time: ${
+        (Date.now() - startTime) / 1000
+      } seconds`
+    );
+
+    console.log(connection);
+    return {
+      capabilities: server.capabilities(),
+    };
+  }
+);
 
 connection.listen();
