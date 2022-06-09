8d556adcf728ec58cf417e2dad47ddfce41ce48e
zuhanit <36349353+zuhanit@users.noreply.github.com>
2022-06-09 18:49:30 +0900

    fix: Remove object type

--- server/src/context/evaluator/literal.ts
+++ server/src/context/evaluator/literal.ts
@@ -1,6 +1,15 @@
-import { ClassSymbol } from '../symbolTable/ClassSymbol';
-import { FunctionSymbol } from '../symbolTable/FunctionSymbol';
-import { ISymbol } from '../symbolTable/ISymbol';
+import { ClassSymbol } from "../symbolTable/ClassSymbol";
+import { FunctionSymbol } from "../symbolTable/FunctionSymbol";
+import { ISymbol } from "../symbolTable/ISymbol";
 
 // eslint-disable-next-line @typescript-eslint/ban-types
-export type Literal = ClassSymbol | FunctionSymbol | object | string | number | boolean | ISymbol | null | undefined;
+export type Literal =
+  | ClassSymbol
+  | FunctionSymbol
+  | string
+  | number
+  | boolean
+  | ISymbol
+  | null
+  | undefined
+  | any;
