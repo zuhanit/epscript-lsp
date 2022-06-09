8ad972c7903a96aa6b35b1c949f8bb9dc367e2a8
zuhanit <36349353+zuhanit@users.noreply.github.com>
2022-06-10 00:25:36 +0900

    feat: Add publish task

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -4,15 +4,14 @@ const webpack = require("webpack-stream");
 const webpackClientConfig = require("../client/webpack.config");
 const webpackServerConfig = require("../server/webpack.config");
 
-const updateeudplib = async (cb) => {
+const updateeudplib = async () => {
   return exec("git submodule update", (err, stdout, stderr) => {
     console.log(stdout);
     console.log(stderr);
-    cb(err);
   });
 };
 
-const buildeudplibData = async (cb) => {
+const buildeudplibData = async () => {
   return spawn("python", ["server/src/lib/collector/test.py"], {
     stdio: "inherit",
   });
@@ -30,8 +29,16 @@ const compileServer = () => {
     .pipe(dest("server/dist"));
 };
 
+const publish = () => {
+  return exec("vsce publish", (err, stdout, stderr) => {
+    console.log(stdout);
+    console.log(stderr);
+  });
+};
+
 task(
   "default",
   series(compileClient, updateeudplib, buildeudplibData, compileServer)
 );
 task("build", series(compileClient, compileServer));
+task("publish", series(compileClient, compileServer, publish));
