44fda75e10ad5ccbaf67ed08282fa7b22ab04241
zuhanit <36349353+zuhanit@users.noreply.github.com>
2022-06-16 16:56:10 +0900

    fix: Remove folderPath

--- server/src/parser.ts
+++ server/src/parser.ts
@@ -1,78 +1,94 @@
-import { CodeCompletionCore } from 'antlr4-c3';
-import { CodePointCharStream, CharStreams, CommonTokenStream, CommonToken, Recognizer, RecognitionException, Token } from 'antlr4ts';
-import { ParseTreeListener } from 'antlr4ts/tree/ParseTreeListener';
-import { ParseTreeWalker } from 'antlr4ts/tree/ParseTreeWalker';
-import { Diagnostic, URI } from 'vscode-languageserver';
-import { TextDocument } from 'vscode-languageserver-textdocument';
-import { BaseListener } from './context/listener/BaseListener';
-import { SymbolTable } from './context/symbolTable/SymbolTable';
-import { epScriptLexer } from './grammar/src/grammar/lib/epScriptLexer';
-import { epScriptParser, ProgramContext } from './grammar/src/grammar/lib/epScriptParser';
+import { CodeCompletionCore } from "antlr4-c3";
+import {
+  CodePointCharStream,
+  CharStreams,
+  CommonTokenStream,
+  CommonToken,
+  Recognizer,
+  RecognitionException,
+  Token,
+} from "antlr4ts";
+import { ParseTreeListener } from "antlr4ts/tree/ParseTreeListener";
+import { ParseTreeWalker } from "antlr4ts/tree/ParseTreeWalker";
+import { Diagnostic, URI } from "vscode-languageserver";
+import { TextDocument } from "vscode-languageserver-textdocument";
+import { BaseListener } from "./context/listener/BaseListener";
+import { SymbolTable } from "./context/symbolTable/SymbolTable";
+import { epScriptLexer } from "./grammar/src/grammar/lib/epScriptLexer";
+import {
+  epScriptParser,
+  ProgramContext,
+} from "./grammar/src/grammar/lib/epScriptParser";
 
-import * as path from 'path';
-import { Analyzer } from './analyzer';
-import { resolve } from 'dns';
-import { ContextSymbolTable } from './context/ContextSymbolTable';
-import { ErrorListener } from './context/listener/ErrorListener';
-import { LexerErrorListener } from './context/listener/LexerErrorListener';
-import { LanguageManager } from './i18n/LanguageManager';
+import * as path from "path";
+import { Analyzer } from "./analyzer";
+import { resolve } from "dns";
+import { ContextSymbolTable } from "./context/ContextSymbolTable";
+import { ErrorListener } from "./context/listener/ErrorListener";
+import { LexerErrorListener } from "./context/listener/LexerErrorListener";
+import { LanguageManager } from "./i18n/LanguageManager";
 
 export interface ParsePackage {
-	symbolTable: ContextSymbolTable;
-	core: CodeCompletionCore;
-	ast: ProgramContext;
-	tokenStream: CommonTokenStream;
-	diagnostic: Diagnostic[];
+  symbolTable: ContextSymbolTable;
+  core: CodeCompletionCore;
+  ast: ProgramContext;
+  tokenStream: CommonTokenStream;
+  diagnostic: Diagnostic[];
 }
 
 export class Parser {
-	/* @internal */
-	private tokenStream: CommonTokenStream = new CommonTokenStream(new epScriptLexer(CharStreams.fromString(''))); // 수정 필요
-	private parser: epScriptParser | undefined;
-	
-	constructor() {
-		this.setText('');
-	}
+  /* @internal */
+  private tokenStream: CommonTokenStream = new CommonTokenStream(
+    new epScriptLexer(CharStreams.fromString(""))
+  ); // 수정 필요
+  private parser: epScriptParser | undefined;
 
-	public static initialize(): Parser {
-		const parser = new Parser();
-		return parser;
-	}
+  constructor() {
+    this.setText("");
+  }
 
-	private setText(document: string) {
-		const charStream = CharStreams.fromString(document);
-		const lexer = new epScriptLexer(charStream);
-		lexer.removeErrorListeners();
-		this.tokenStream = new CommonTokenStream(lexer);
-	}
+  public static initialize(): Parser {
+    const parser = new Parser();
+    return parser;
+  }
 
-	public parse
-		(
-			document: TextDocument,
-			folderPath: string, 
-			analyzer: Analyzer,
-			languageManager: LanguageManager,
-		): ParsePackage
-	{
-		this.setText(document.getText());
-		this.tokenStream.seek(0);
-		this.parser = new epScriptParser(this.tokenStream);
+  private setText(document: string) {
+    const charStream = CharStreams.fromString(document);
+    const lexer = new epScriptLexer(charStream);
+    lexer.removeErrorListeners();
+    this.tokenStream = new CommonTokenStream(lexer);
+  }
 
-		const diagnostics: Diagnostic[] = [];
-		const errorListener = new ErrorListener(diagnostics);
-		this.parser.removeErrorListeners();
-		this.parser.addErrorListener(errorListener);
+  public parse(
+    document: TextDocument,
+    analyzer: Analyzer,
+    languageManager: LanguageManager
+  ): ParsePackage {
+    this.setText(document.getText());
+    this.tokenStream.seek(0);
+    this.parser = new epScriptParser(this.tokenStream);
 
-		const tree = this.parser.program();
-		const listener = new BaseListener(this, document, folderPath, analyzer, diagnostics, languageManager);
+    const diagnostics: Diagnostic[] = [];
+    const errorListener = new ErrorListener(diagnostics);
+    this.parser.removeErrorListeners();
+    this.parser.addErrorListener(errorListener);
 
-		ParseTreeWalker.DEFAULT.walk(listener as ParseTreeListener, tree);
-		return {
-				ast: tree,
-				core: new CodeCompletionCore(this.parser!),
-				symbolTable: listener.symbolTable,
-				tokenStream: this.tokenStream,
-				diagnostic: diagnostics,
-		};
-	}
+    const tree = this.parser.program();
+    const listener = new BaseListener(
+      this,
+      document,
+      analyzer,
+      diagnostics,
+      languageManager
+    );
+
+    ParseTreeWalker.DEFAULT.walk(listener as ParseTreeListener, tree);
+    return {
+      ast: tree,
+      core: new CodeCompletionCore(this.parser!),
+      symbolTable: listener.symbolTable,
+      tokenStream: this.tokenStream,
+      diagnostic: diagnostics,
+    };
+  }
 }
