13cd6da9de83dc66d55e38a2041ae899c703ec33
zuhanit <36349353+zuhanit@users.noreply.github.com>
2022-08-23 15:40:10 +0900

    fix: Override getAllSymbols to avoid infinite loop

--- server/src/context/symbolTable/ModuleSymbol.ts
+++ server/src/context/symbolTable/ModuleSymbol.ts
@@ -1,9 +1,14 @@
+import { ISymbol } from "./ISymbol";
 import { SymbolWithScope } from "./SymbolWithScope";
 import { Type } from "./Type";
 import { TypedSymbol } from "./TypedSymbol";
 
 export class ModuleSymbol extends SymbolWithScope implements TypedSymbol {
   type: Type = { name: "module" };
+
+  public getAllSymbols(): ISymbol[] {
+    return [];
+  }
 
   public static isModule(arg: any): arg is ModuleSymbol {
     return arg instanceof ModuleSymbol;
