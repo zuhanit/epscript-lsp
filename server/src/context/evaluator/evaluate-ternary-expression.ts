c6972363f95907548776490c3c3098a4fe103acc
Zuhanit <zuhanit3@gmail.com>
2022-10-07 22:54:43 +0900

    fix: Check expression is complete

--- server/src/context/evaluator/evaluate-ternary-expression.ts
+++ server/src/context/evaluator/evaluate-ternary-expression.ts
@@ -6,16 +6,21 @@ export function evaluateTernaryExpression({
   node,
   ...rest
 }: EvaluatorOption<TernaryExpressionContext>) {
+  const singleExpressions = node.singleExpression();
+  if (singleExpressions.length !== 3) {
+    return undefined;
+  }
+
   const conditionalValue = evaluateNode({
-    node: node.singleExpression(0),
+    node: singleExpressions[0],
     ...rest,
   });
 
   // 조건이 참일 경우 첫번째 노드 표현식을 평가한 값을 반환
   if (conditionalValue) {
-    return evaluateNode({ node: node.singleExpression(1), ...rest });
+    return evaluateNode({ node: singleExpressions[1], ...rest });
   }
 
   // 거짓일 경우 두번째 값을 반환
-  return evaluateNode({ node: node.singleExpression(2), ...rest });
+  return evaluateNode({ node: singleExpressions[2], ...rest });
 }
