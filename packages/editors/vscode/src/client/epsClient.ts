711ae8b7f366bd81ba246b771a8f59a4d1570389
zuhanit <36349353+zuhanit@users.noreply.github.com>
2022-10-28 16:58:03 +0900

    feat: Refactor extension.ts

---
+++ packages/editors/vscode/src/client/epsClient.ts
@@ -0,0 +1,85 @@
+import {
+  commands,
+  ExtensionContext,
+  window,
+  workspace,
+  WorkspaceFolder,
+} from "vscode";
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  TransportKind,
+} from "vscode-languageclient/node";
+import path from "path";
+import { OffsetManager } from "./offsetManager";
+import { BuildManager, BuildScript } from "./buildManager";
+
+export class EPSClient {
+  public static create(context: ExtensionContext): LanguageClient {
+    const client = new LanguageClient(
+      "epScript",
+      EPSClient.createServerOptions(context.extensionPath),
+      EPSClient.createClientOptions()
+    );
+
+    const workspaceFolder = workspace.workspaceFolders;
+    if (workspaceFolder) {
+      const workspaces: WorkspaceFolder[] = workspaceFolder.map((x) => {
+        return {
+          index: x.index,
+          name: x.name,
+          uri: x.uri,
+        };
+      });
+      const offsetManager = new OffsetManager();
+      const buildManager = new BuildManager(workspaces);
+
+      context.subscriptions.push(
+        window.registerTreeDataProvider("euddraftBuild", buildManager),
+        commands.registerCommand("euddraftBuild.refresh", () =>
+          buildManager.refresh()
+        ),
+        commands.registerCommand("euddraftBuild.run", (node: BuildScript) =>
+          buildManager.build(node)
+        ),
+        commands.registerCommand("euddraftBuild.edit", (node: BuildScript) =>
+          buildManager.edit(node)
+        ),
+        commands.registerTextEditorCommand("epscript.offsets", async (editor) =>
+          offsetManager.main(editor)
+        )
+      );
+    }
+    console.log("[eps-server] Client Loaded.");
+
+    return client;
+  }
+
+  public static createServerOptions(extensionPath: string): ServerOptions {
+    const serverModule = path.join(extensionPath, "dist", "extension.js");
+    const debugArgv = ["--nolazy", "--inspect=6009"];
+
+    return {
+      run: {
+        module: serverModule,
+        transport: TransportKind.ipc,
+      },
+      debug: {
+        module: serverModule,
+        transport: TransportKind.ipc,
+        options: {
+          execArgv: debugArgv,
+        },
+      },
+    };
+  }
+  public static createClientOptions(): LanguageClientOptions {
+    return {
+      documentSelector: [
+        { scheme: "file", language: "epscript" },
+        { scheme: "untitled", language: "epscript" },
+      ],
+    };
+  }
+}
--- packages/editors/vscode/src/extension.ts
+++ packages/editors/vscode/src/extension.ts
@@ -3,92 +3,18 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
 
-import * as path from "path";
-import {
-  workspace,
-  ExtensionContext,
-  window,
-  WorkspaceFolder,
-  commands,
-} from "vscode";
+import { ExtensionContext } from "vscode";
 
-import {
-  LanguageClient,
-  LanguageClientOptions,
-  ServerOptions,
-  TransportKind,
-} from "vscode-languageclient/node";
-import { BuildManager, BuildScript } from "./client/buildManager";
-import { OffsetManager } from "./client/offsetManager";
+import { LanguageClient } from "vscode-languageclient/node";
+import { EPSClient } from "./client/epsClient";
 
 let client: LanguageClient;
 
 export function activate(context: ExtensionContext) {
-  // The server is implemented in node
-  const serverModule = context.asAbsolutePath(path.join("dist", "server.js"));
-  // The debug options for the server
-  // --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
-  const debugOptions = { execArgv: ["--nolazy", "--inspect=6009"] };
-
-  // If the extension is launched in debug mode then the debug server options are used
-  // Otherwise the run options are used
-  const serverOptions: ServerOptions = {
-    run: { module: serverModule, transport: TransportKind.ipc },
-    debug: {
-      module: serverModule,
-      transport: TransportKind.ipc,
-      options: debugOptions,
-    },
-  };
-
-  // Options to control the language client
-  const clientOptions: LanguageClientOptions = {
-    documentSelector: [
-      { scheme: "file", language: "epscript" },
-      { scheme: "untitled", language: "epscript" },
-    ],
-    synchronize: {
-      // Notify the server about file changes to '.clientrc files contained in the workspace
-      fileEvents: workspace.createFileSystemWatcher("**/.clientrc"),
-    },
-  };
-
-  // Create the language client and start the client.
-  client = new LanguageClient(
-    "epScriptLanguageClient",
-    "epScript Language Client",
-    serverOptions,
-    clientOptions
-  );
-
-  // Start the client. This will also launch the server
-  client.start();
-  const workspaceFolder = workspace.workspaceFolders;
-  if (workspaceFolder) {
-    const workspaces: WorkspaceFolder[] = workspaceFolder.map((x) => {
-      return {
-        index: x.index,
-        name: x.name,
-        uri: x.uri,
-      };
-    });
-    const offsetManager = new OffsetManager();
-    const buildManager = new BuildManager(workspaces);
-    window.registerTreeDataProvider("euddraftBuild", buildManager);
-    commands.registerCommand("euddraftBuild.refresh", () =>
-      buildManager.refresh()
-    );
-    commands.registerCommand("euddraftBuild.run", (node: BuildScript) =>
-      buildManager.build(node)
-    );
-    commands.registerCommand("euddraftBuild.edit", (node: BuildScript) =>
-      buildManager.edit(node)
-    );
-    commands.registerTextEditorCommand("epscript.offsets", async (editor) =>
-      offsetManager.main(editor)
-    );
-  }
-  console.log("[eps-server] Client Loaded.");
+  const client = EPSClient.create(context);
+  client
+    .start()
+    .catch((err) => client.error("Starting client failed.", err, "force"));
 }
 
 export function deactivate(): Thenable<void> | undefined {
