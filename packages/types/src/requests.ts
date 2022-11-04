62bded416d858a92f8299392979565d51146d3e7
zuhanit <36349353+zuhanit@users.noreply.github.com>
2022-11-04 20:48:25 +0900

    feat: Add config requests

---
+++ packages/types/src/requests.ts
@@ -0,0 +1,47 @@
+import { RequestType } from "vscode-jsonrpc";
+
+export type NoConfigParams = {
+  message: string;
+};
+
+/**
+ * The NoConfigRequest is sent from the server to the client to inform
+ * the client that no euddraft configuration file.
+ */
+export namespace NoConfigRequest {
+  export const method = "epscript/noConfig" as const;
+  export const type = new RequestType<NoConfigParams, void, void>(method);
+}
+
+export type MultipleConfigParams = {
+  message: string;
+};
+
+export type MultipleConfigResult = {
+  //
+};
+
+/**
+ * The MultipleConfigRequest is sent from the server to the client to inform
+ * the client that have multiple euddraft configuration file.
+ *
+ * Since euddraft has no default config file(ex: package.json, cargo.toml), the
+ * server collects all euddraft files.
+ */
+export namespace MultipleConfigRequest {
+  export const method = "epscript/multipleConfig" as const;
+  export const type = new RequestType<
+    MultipleConfigParams,
+    MultipleConfigResult,
+    void
+  >(method);
+}
+
+export type SelectConfigParams = {
+  config: string;
+};
+
+export namespace SelectConfigRequest {
+  export const method = "epscript/selectConfig" as const;
+  export const type = new RequestType<SelectConfigParams, void, void>(method);
+}
