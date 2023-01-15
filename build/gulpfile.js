7ba951820aff5429c040f5af7d1976c507961cf7
zuhanit <36349353+zuhanit@users.noreply.github.com>
2023-01-16 04:36:04 +0900

    test: Move error to test

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -13,10 +13,10 @@ const updateeudplib = () => {
   });
   app.stderr.on("data", (err) => {
     console.log("Error: ", err.toString());
-    throw new Error("Error caused while update eudplib");
   });
   app.stderr.on("end", (err) => {
     console.log(err.toString());
+    throw new Error("Error caused while update eudplib");
   });
   return app;
 };
